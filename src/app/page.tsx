6f3f01e4fede4cd74ff7f2fbb67432c2a43153d5
shadekiller666 <ezioauditorerevs@gmail.com>
2025-05-12 16:42:50 -0700

    Deployment test!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       This is a website!
+      And its deployed! :D
     </main>
   );
 }
